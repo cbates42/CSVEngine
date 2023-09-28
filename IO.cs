edb1d378b779e5a7cc44bfd9d1ac91cb65e39a51
Cameron <maxuszzzx@gmail.com>
2023-09-28 17:26:46 -0500

    Initial commit

---
+++ IO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVEngine
+{
+    internal class IO
+    {//                                             
+      
+        public IO() 
+        {
+            //                                              v  add whatever the directory is here later
+            var path = Directory.GetCurrentDirectory();
+
+            using(StreamReader sr = new StreamReader(path))
+            {
+                var line = sr.ReadLine();
+                while(line != null)
+                {
+                    var stats = line.Split("|");
+
+                    foreach (var item in stats)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    line = sr.ReadLine();
+                }
+                   
+            }
+
+            Console.Read();
+        }
+    
+
+
+
+
+
+
+
+
+    }
+}
